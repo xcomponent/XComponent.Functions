72b57bbb1f3c8672f319d906715bdbf8577c429b
Marcos Almeida <marcos.almeida@xcomponent.com>
2018-01-11 09:46:53 +0100

    Timeout is optional

--- src/XComponent.Functions/Core/FunctionsConfiguration.cs
+++ src/XComponent.Functions/Core/FunctionsConfiguration.cs
@@ -2,7 +2,7 @@ namespace XComponent.Functions.Core
 {
     public class FunctionsConfiguration
     {
-        public long TimeoutInMillis { get; set; }
+        public long? TimeoutInMillis { get; set; }
     }
 }
 
